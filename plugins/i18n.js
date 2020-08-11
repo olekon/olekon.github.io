1b4b8d529988790aef1d921d315396a580e845ca
olekon <ok@naulab.pro>
2020-08-11 22:45:42 +0700

    refresh scroll on lang change

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -1,5 +1,6 @@
 export default function ({ app, store }) {
     app.i18n.onLanguageSwitched = () => {
+        app.$scrollTrigger.refresh();
         store.dispatch('projects/create', app.i18n);
     };
 }
